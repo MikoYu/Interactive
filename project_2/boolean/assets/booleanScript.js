8b32713f7c2a145c137cf332b1cffbb87b944aed
Mingke Yu <mingke.yu.miko@gmail.com>
2018-04-01 01:42:08 -0700

    Boolean project update
    - rotating animation added
    - debounce problem fixed

--- project_2/boolean/assets/booleanScript.js
+++ project_2/boolean/assets/booleanScript.js
@@ -4,17 +4,17 @@ $(document).ready(function(){
   var interval = Math.round(Math.random() * 4000 + 1000);
   var tid = setInterval(newSpot, interval);
 
-  //add a new spot to the div "spots" after a random interval
+  //add a new spot to the div 'spots' after a random interval
   function newSpot() {
 
     // calculate the position and size of the new spot
-    var posX = Math.round(Math.random() * (screen.width - 100));
-    var posY = Math.round(Math.random() * (screen.height - 100));
+    var posX = Math.round(Math.random() * ((screen.width + screen.height)/2));
+    var posY = Math.round(Math.random() * ((screen.width + screen.height)/2));
     var size = Math.round(Math.random() * 50 + 20);
 
     // add the new spot and show it on the screen; and duplicate it
-    $('#spots, #spots2').append(
-      $('<div class="spot"></div>').css({
+    $('#spots1, #spots2').append(
+      $("<div class='spot'></div>").css({
         top: posY + 'px',
         left: posX + 'px',
         width: size + 'px',
@@ -23,100 +23,92 @@ $(document).ready(function(){
     );
 
     console.log('new spot added');
-
-    // this failed qvq
-    // duplicate the spots (at same positions) for their disappearing animation
-    // var existingContents = $("#spots").html();
-    // $("#spots2").append(existingContents);
   }
 
-
   // clear all the spots on the screen when the mouse moves
-  var mousemove_timeout = null;
+  // also, detect mouse movement, map the movement of the spots to the mouse's
+  var timeout_rotate = window.setTimeout(spinning, 30000);
+  var last_position = {};
+  var deltaX = 0;
+  var deltaY = 0;
 
-  $(document).mousemove(function() {
-    if (mousemove_timeout == null) {
-      mousemove_timeout = window.setTimeout(clearSpots, 1000);
+  $(document).mousemove( $.throttle(2000, true, function(event) {
+    // stop rotating, restore timer
+    $('#spots1, #spots2').css('animation-play-state', 'paused');
+    timeout_rotate = window.setTimeout(spinning, 30000);
+
+    // move with the mouse
+    if (typeof(last_position.x) != 'undefined') {
+      //get the change from last position to this position
+      deltaX = last_position.x - event.clientX;
+      deltaY = last_position.y - event.clientY;
     }
-  });
 
-  function clearSpots() {
+    last_position = {
+      x : event.clientX,
+      y : event.clientY
+    };
 
-    $('body').css("background", "#ffffff");
+    clearSpots(deltaX, deltaY);
 
-    $('#spots > .spot').animate({
-      top: '-=10px',
-      left: '-=10px', //moves up left
+  }));
+
+  function spinning(){
+    console.log('spinning');
+    $('#spots1, #spots2').css('animation-play-state', 'running');
+  };
+
+  function clearSpots(xMovement, yMovement) {
+    console.log('clearSpots');
+    // change colors, split, etc.
+    $('body').css('background', '#ffffff');
+
+    // var xMovement = Math.pow(x, 1/3);
+    // var yMovement = Math.pow(y, 1/3);
+
+    $('#spots1 > .spot').animate({
+      marginTop: '-=' + yMovement,
+      marginLeft: '-=' + xMovement,
       backgroundColor: '#ff0000',
     });
 
     $('#spots2 > .spot').animate({
-      top: '+=10px',
-      left: '+=10px', //moves down right
+      marginTop: '+=' + yMovement,
+      marginLeft: '+=' + xMovement,
       backgroundColor: '#ffff00'
     });
 
     $('#text1 > p').animate({
-      marginTop: '-=10px',
-      marginLeft: '-=10px', //moves up left
+      marginTop: '-=' + yMovement,
+      marginLeft: '-=' + xMovement,
       color: '#ff0000',
     });
 
     $('#text2 > p').animate({
-      marginTop: '+=10px',
-      marginLeft: '+=10px', //moves up left
+      marginTop: '+=' + yMovement,
+      marginLeft: '+=' + xMovement,
       color: '#ffff00',
     });
 
-    $('#spots, #spots2, #text1, #text2').children().fadeOut(500, function() {
-      $('#spots, #spots2, #text1, #text2').empty();
+    // clear spots and text
+    $('#spots1, #spots2, #text1, #text2').children().fadeOut(500, function() {
+      $('#spots1, #spots2, #text1, #text2').empty();
     });
 
-    delay = 1000;
+    console.log('mouse moving, spots removed');
+
+    // add background and text as it was at the beginning
+    delayRestore = 1000;
     setTimeout(function(){
-      $('body').css("background", "#000000");
+      $('body').css('background', '#000000');
       $('#text1, #text2').append('<p>WAIT</p>');
       $('p').css({
         'color': '#ffffff',
         'marginTop': '0',
         'marginLeft': '0'
       });
-
-    }, delay);
-
-    console.log('mouse moving, spots removed');
-
-    window.clearTimeout(mousemove_timeout);
-    mousemove_timeout = null;
+    }, delayRestore);
 
   };
 
-
 });
-
-
-
-
-//hitory version, without really listening for mousemove
-// $(document).on('mousemove', function() {
-//   clearTimeout(timeout);
-//
-//   for (var time = 0; time < 5; time++ ){
-//
-//     timeout = setTimeout(function() {
-//       var posX = Math.round(Math.random() * (screen.width - 100));
-//       var posY = Math.round(Math.random() * (screen.height - 100));
-//       var size = Math.round(Math.random() * 50 + 20);
-//
-//       $("body").append(
-//         $('<div class = "spot"></div>').css({
-//           top: posY + 'px',
-//           left: posX + 'px',
-//           width: size + 'px',
-//           height: size + 'px',
-//         })
-//       );
-//       console.log('Mouse idle for' + time/1000 + 'sec');
-//     }, 3000);
-//   }
-// });
